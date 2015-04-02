89091f4ee1ac7791a6db8ab8a515b131ba92000b
Georgy5 <kgk86@hotmail.com>
2015-04-02 07:16:51 +0200

    Week 1 Day 3 files - examples and homework

---
+++ Week_1_Ruby/Day3_blank.rb
@@ -0,0 +1,15 @@
+class Nilclass
+	def blank?
+		true
+	end
+end
+
+class String
+	def blank?
+		self.size == 0
+	end
+end
+
+["", "person", nil].each do |element|
+	puts element unless element.blank?
+end
