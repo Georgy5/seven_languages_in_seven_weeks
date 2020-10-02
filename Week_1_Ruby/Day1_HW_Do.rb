deaf56f42593051bdf8535e1430c057f196c6078
Karl Keller <kgk86@hotmail.com>
2020-10-02 03:07:46 +0200

    Corrected typos

--- Week_1_Ruby/Day1_HW_Do.rb
+++ Week_1_Ruby/Day1_HW_Do.rb
@@ -10,12 +10,12 @@ substituteT("tial", "ezek")
 # Print the string "Hello world"
 puts "Hello world"
 
-# Find the index or "Ruby" for the string "Hello, Ruby"
-puts "Hello, Rubz".index "Ruby"
+# Find the index for "Ruby" for the string "Hello, Ruby"
+puts "Hello, Ruby".index "Ruby"
 
 # Print the string "This is sentence number 1" where 1 changes from 1 to 10
 (1..10).each { |i| puts "This is sentence number #{i}" }
 
 # Run a Ruby program from a file:
-#  - run this file in the command line bz typing 
+#  - run this file in the command line by typing 
 #	"ruby Day1_HW_Do.rb"	and hit Enter
